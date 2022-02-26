e50932c408e338a67639240f3c46ca388b2c8925
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-02-25 21:26:28 -0500

    Start working on components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import AddListItem from './components/AddListItem';
+import ListItems from './components/ListItems';
+
 
 function App() {
   return (
-    <div className="App">     
+    <div className="app">
+      <div className="app-container">
+        <h1>To Do List</h1>
+        <AddListItem />
+        <ListItems />
+      </div>
     </div>
   );
 }
