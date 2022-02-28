7f6ecc7a2d07ae0dedac1cf57870082f183165f8
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-02-28 17:40:37 -0500

    Implement rendering task data into ListItemComponent

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ListItemComponent from './ListItem';
 import List from '@mui/material/List';
 
-const ListItems = () => {
+const ListItems = ({ tasks, setTasks }) => {
+  //get task from tasks
+  const taskElement = tasks.map(task => {
+    return (
+      <ListItemComponent 
+        task={task}
+        tasks={tasks}
+        setTasks={setTasks}
+        key={task.id}
+      />
+    )
+  })
+
   return (
     <List sx={{ width: '50vw' }}>
-      <ListItemComponent />
-      <ListItemComponent />
-      <ListItemComponent />
-      <ListItemComponent />
+      {taskElement}
     </List>
   )
 }
