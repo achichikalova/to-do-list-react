b300640c3ad8fd57fb41df85bcb61d0b81f7e102
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-02-28 17:41:52 -0500

    Start working on functionality, initialize states and send them as props into components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 import AddListItem from './components/AddListItem';
 import ListItems from './components/ListItems';
 
 
 function App() {
+
+  const [userInput, setUserInput] = useState('');
+  const [tasks, setTasks] = useState([]);
+
   return (
     <div className="app">
       <div className="app-container">
         <h1>To Do List</h1>
-        <AddListItem />
-        <ListItems />
+        <AddListItem
+          userInput={userInput}
+          setUserInput={setUserInput}
+          tasks={tasks}
+          setTasks={setTasks}
+        />
+        <ListItems
+          tasks={tasks}
+          setTasks={setTasks}
+        />
       </div>
     </div>
   );
