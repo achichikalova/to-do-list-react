0c55d29d085de061284bd39388a2510d9d6d2829
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-02-28 14:51:29 -0500

    Add material UI styles to AddListItem component

--- src/components/AddListItem.js
+++ src/components/AddListItem.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 const AddListItem = () => {
   return (
-    <div className='addListItem-container'>
-      <input type='text' placeholder='Print your task here...' />
-      <AddCircleIcon className='add-btn'/>
-    </div>
+    <Box
+      component="form"
+      sx={{
+        m: 2,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField 
+        id="standard-basic" 
+        label="Print Your Task Here..." variant="standard"
+        fullWidth />
+      <AddCircleIcon 
+        sx={{
+          width: '10%',
+          height: '2.2rem',
+          color: 'green',
+          cursor: 'pointer'          
+        }} 
+        className='add-btn'/>
+    </Box>
   )
 }
 
