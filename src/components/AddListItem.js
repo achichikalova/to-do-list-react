3a314e0ea501edef1982ffc076a0140c85e83fac
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-03-01 11:35:46 -0500

    Add user input validation, send error message if user input empty string

--- src/components/AddListItem.js
+++ src/components/AddListItem.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 const AddListItem = ({ userInput, setUserInput, tasks, setTasks }) => {
 
+  const [error, setError] = useState(null);
+
   const inputHandler = (e) => {
     setUserInput(e.target.value);
+    setError(null);
   }
 
   const submitTaskHandler = (e) => {
     e.preventDefault();
-    setTasks([
-      ...tasks, 
-      {
-        text: userInput,
-        id: Math.random().toString(36).substr(2, 9),
-        complete: false
-      }
-    ]);
+    
+    if (userInput.length !== 0) {
+      setTasks([
+        ...tasks, 
+        {
+          text: userInput,
+          id: Math.random().toString(36).substr(2, 9),
+          complete: false
+        }
+      ]);
+    } else {
+      setError("Please, print your task first")
+    }
     setUserInput('');
   }
 
@@ -34,12 +42,17 @@ const AddListItem = ({ userInput, setUserInput, tasks, setTasks }) => {
       noValidate
       autoComplete="off"
     >
-      <TextField 
+      <TextField
         id="standard-basic" 
-        label="Print Your Task Here..." variant="standard"
+        label="Print Your Task Here..." 
+        variant="standard"
         fullWidth
         onChange={inputHandler}
         value={userInput}
+        multiline
+        maxRows={4}
+        {...(error && {error:!null})}
+        helperText={error}
       />
       <AddCircleIcon 
         sx={{
