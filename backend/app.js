fdf4de0295db2f12f4adba9c6d730535cf5d620b
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-03-11 15:32:56 -0500

    Set the db and add it to backend

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 
+// Connect db
+const { Pool } = require('pg');
+const dbConfig = require('./dbConfig');
+const db = new Pool(dbConfig);
+
 app.get('/', (req, res) => {
   res.send("Test");
 })
 
+
+
 app.listen(3001, () => {
-  console.log('Listening on PORT 3001');
+  console.log('ToDo app listening on PORT 3001');
 })
