3290762424671b3fe4505e4178d75ecc57783811
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-03-12 11:24:54 -0500

    Implement API route for tasks

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 // Connect db
 const { Pool } = require('pg');
 const dbConfig = require('./dbConfig');
 const db = new Pool(dbConfig);
 
 app.get('/', (req, res) => {
-
+  res.send('Test');
 })
 
+app.get('/api/tasks', (req, res) => {
+  const query = `SELECT * FROM tasks;`;
 
+  db.query(query)
+    .then(data => {
+      const tasks = data.rows;
+      res.status(200).json({ tasks });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500)
+         .json({ error: err.message });
+    })
+})
 
 app.listen(3001, () => {
   console.log('ToDo app listening on PORT 3001');
