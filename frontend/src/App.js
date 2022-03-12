fd6a50523dc952812726e6bf23d170328a731f8a
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-03-12 13:18:39 -0500

    Implement rendering tasks from API server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import AddListItem from './components/AddListItem';
 import ListItems from './components/ListItems';
+import axios from 'axios';
 
 
 function App() {
 
   const [userInput, setUserInput] = useState('');
   const [tasks, setTasks] = useState([]);
+  console.log(tasks)
+
+  useEffect(() => {
+    const tasksAPI = `http://localhost:3001/api/tasks`;
+
+    axios
+      .get(tasksAPI)
+      .then((res) => {
+        const tasksData = res.data.tasks;
+        if(tasksData) {
+          setTasks(tasksData);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, []);
 
   return (
     <div className="app">
